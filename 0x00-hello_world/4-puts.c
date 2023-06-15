cb1c55e584bd3141278267fff62caa4438200807
Ahmed Hesham <ahisham108@gmail.com>
2023-06-15 11:53:47 -0700

    PUTS

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main Entry point
+ *
+ * Description that prints with puts function
+ *
+ * return always zero (Sucsess)
+*/
+ 
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
