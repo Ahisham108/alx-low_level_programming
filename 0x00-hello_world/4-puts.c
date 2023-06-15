2c0247aa2398ac1fff4e7f0a178f03ccac5d1b73
Ahmed Hesham <ahisham108@gmail.com>
2023-06-15 12:12:13 -0700

    h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- *main Entry point
+ * main - Entry point
  *
- * Description that prints with puts function
+ * Description: 'A C Programe that prints with puts function'
  *
- * return always zero (Sucsess)
+ * Return: Always 0 (Success)
 */
- 
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+        puts("\"Programming is like building a multilingual puzzle");
+        return 0;
 }
