61540a8520d07f425ad1e353c044aca37d264d5e
Ahmed Hesham <ahisham108@gmail.com>
2023-06-15 12:31:04 -0700

    Hello Betty

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C Programe that prints with print f function
+ *
+ * Return: Always 0 (Sucsess)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
