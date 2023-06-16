607bdc88bc0934561528fbe14b7870e177975cc5
Ahmed Hesham <ahisham108@gmail.com>
2023-06-16 10:06:51 -0700

    a

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print number of 16 in lowercase
+ *
+ * Return: 0 always (sucsess)
+*/
+
+int main(void)
+{
+	int degit = 48;
+
+	while (digit <= 102)
+	{
+		putchar (digit);
+		if (digit == 57)
+			digit += 39;
+		digit++;
+	}
+	putchar ('n');
+	return (0);
