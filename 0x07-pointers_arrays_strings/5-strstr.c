e144ff05f6d9985e3df3c7dd679fb65f1479ee6b
Ahmed Hesham <ahisham108@gmail.com>
2023-07-03 10:00:25 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_cheeseboard - prints cheese board
+ *
+ * @a: rows
+ * return: nothing
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int itr, jtr;
+
+	for (itr = 0; itr < 8; itr++)
+	{
+		for (jtr = 0; jtr < 8; jtr++)
+		{
+			_putchar(a[itr][jtr]);
+		}
+		_putchar('\n')
+	}
+}
