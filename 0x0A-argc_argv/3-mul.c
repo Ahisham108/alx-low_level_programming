4ea92250adf638ad580a624182424359e88e83c0
Ahmed Hesham <ahisham108@gmail.com>
2023-07-07 10:43:38 -0700

    F-Betty

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ *main-prints sum of 2 numbers.
+ *@argc: number of command line arguments.
+ *@argv: pointer to an array of command line arguments.
+ *Return: 0 sucsess 1 not sucsess
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
